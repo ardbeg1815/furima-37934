393637d6f2de8574ff2fd2ec106f42d33baf2556
ardbeg1815 <zhengshugaoyuan85@gmail.com>
2022-09-20 20:10:23 +0900

    モデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  validates :item_name,   presence: true
+  validates :description, presence: true
+  validates :price,       presence: true
+            
+  varidates :category_id,        numericality: { other_than: 1 }
+  validates :condition_id,       numericality: { other_than: 1 }
+  validates :delivery_charge_id, numericality: { other_than: 1 }
+  validates :prefecture_id,      numericality: { other_than: 1 }
+  validates :delivery_date_id,   numericality: { other_than: 1 }
 end
