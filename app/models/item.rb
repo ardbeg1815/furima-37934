b335357c03a6d354396a4e90723999f49b08153e
ardbeg1815 <zhengshugaoyuan85@gmail.com>
2022-09-20 23:29:00 +0900

    エラーメッセージの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,18 @@
 class Item < ApplicationRecord
-  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
-    validates :price, numericality: { only_integer: true, greater_than_or_equel_to: 300, less_than_or_equel_to: 9_999_999 }
-  end
 
   validates :image,              presence: true
   validates :item_name,          presence: true
   validates :description,        presence: true
-  validates :category_id,        numericality: { other_than: 1 }
-  validates :condition_id,       numericality: { other_than: 1 }
-  validates :delivery_charge_id, numericality: { other_than: 1 }
-  validates :prefecture_id,      numericality: { other_than: 1 }
-  validates :delivery_date_id,   numericality: { other_than: 1 }
+  validates :category_id,        numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id,       numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id,      numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_date_id,   numericality: { other_than: 1, message: "can't be blank" }
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+    presence: { message: "can't be blank" }
+  end
+
 
   has_one_attached :image
   belongs_to :user
