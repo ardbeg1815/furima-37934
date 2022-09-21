025230f17a2015cb36bd2c2317db6ea82490cd2e
ardbeg1815 <zhengshugaoyuan85@gmail.com>
2022-09-21 19:32:45 +0900

    テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -70,5 +70,11 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
     end
+    it "Userが紐付いてなければ登録できない" do
+      @item.user = nil
+      @item.valid?
+      #binding.pry
+      expect(@item.errors.full_messages).to include("User must exist")
+    end
   end
 end
