1fb0290b05cc4539820e5aed74263965979a3a48
ardbeg1815 <zhengshugaoyuan85@gmail.com>
2022-09-22 16:41:42 +0900

    showページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :delivery_date_id,:price, :image).merge(user_id: current_user.id)
