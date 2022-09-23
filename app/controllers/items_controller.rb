1ca26a322f7762a946009ef8f515bb96c4aabde0
ardbeg1815 <zhengshugaoyuan85@gmail.com>
2022-09-23 14:08:21 +0900

    コントローラー、ルートの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,19 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :delivery_date_id,:price, :image).merge(user_id: current_user.id)
