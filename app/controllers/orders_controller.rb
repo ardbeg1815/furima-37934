613b182b5e9755dff1b7165f62510c1170f4e77e
ardbeg1815 <zhengshugaoyuan85@gmail.com>
2022-09-29 02:05:31 +0900

    商品購入機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,10 +2,35 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @donation = Donation.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @donation = Donation.new(donation_params)
+    binding.pry
+    if @donation.valid?
+      pay_item
+      @donation.save
+      redirect_to root_path
+    else
+      #@item = Item.find(params[:item_id])
+      render :index
+    end
   end
 
   private
-  def order_params
-    params.require( :order ).merge( user_id: current_user.id )
+
+  def donation_params
+    params.require(:donation).permit(:price, :post_code, :prefecture_id, :municipalities, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id,token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_dd6ef05a179ed8d89b847189" 
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: donation_params[:token],
+      currency: 'jpy'
+    )
   end
 end
---
+++ app/javascript/packs/card.js
@@ -0,0 +1,35 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_9e8cf1b3e3ad965b92ee7b94");
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("donation[number]"),
+      cvc: formData.get("donation[cvc]"),
+      exp_month: formData.get("donation[exp_month]"),
+      exp_year: `20${formData.get("donation[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
+    });
+  });
+};
+
+window.addEventListener("load", pay);
